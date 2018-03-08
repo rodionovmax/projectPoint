8847ee4fa378e5428345aeae2f8c057ff1ef0a9d
rodionovmax90test@gmail.com <rodionovmax90test@gmail.com>
2018-03-08 18:55:44 -0500

    Изменил класс Point в котором теперь 2 объекта
    Удалил классы Point1, Point 2. Изменил класс Point в котором теперь 2 объекта класса

--- src/main/java/src/com/company/Point.java
+++ src/main/java/src/com/company/Point.java
@@ -2,30 +2,25 @@ package com.company;
 
 public class Point {
 
-    public double x1;
-    public double x2;
-    public double y1;
-    public double y2;
+  public double x;
+  public double y;
 
-    //создаем конструктор
-    public Point(double x1, double y1, double x2, double y2){
-      this.x1 = x1;
-      this.y1 = y1;
-      this.x2 = x2;
-      this.y2 = y2;
-    }
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
 
-    public double distance2 () {
-      return Math.sqrt(Math.pow((this.x1 - this.x2), 2) + Math.pow((this.y1 - this.y2), 2));
-    }
+  public static double distance (Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
+  }
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-      Point p = new Point(3,4,6,-5);
+    Point p1 = new Point(4, 3);
+    Point p2 = new Point(7, -1);
 
-      System.out.println("Расстояние между точкой 1 с координатами (" + p.x1 + ", " + p.y1 +") и точкой 2 " +
-              "с координатами (" + p.x2 + ", " + p.y2 + ") равно " + p.distance2());
+    System.out.println("Расстояние между точкой 1 с координатами (" + p1.x + ", " + p1.y +") и точкой 2 " +
+            "с координатами (" + p1.x + ", " + p2.y + ") равно " + distance(p1, p2));
 
-    }
-
+  }
 }
